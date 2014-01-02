1deeb27baaae592cea29626a8910c7bb0545800a
Charles Anthony <charles.unix.pro@gmail.com>
2014-01-02 13:58:36 -0800

    Keep up to date with latest models

---
+++ src/dps8/dps8_iom.h
@@ -0,0 +1,23 @@
+// Used to communicate between the IOM and devices
+typedef struct {
+    int iom_unit_num;
+    int chan;
+    void* statep; // For use by device specific code
+    int dev_cmd; // 6 bits
+    // XXX Caution; this is the unit number from the IOM, it is NOT the
+    // same as unit number. E.g.
+    //    dev_code       simh
+    //      0            cardreader[0]
+    //      1            tape[0]
+    //      2            tape[1]
+    //      3            disk[0]
+
+    int dev_code; // 6 bits
+    int chan_data; // 6 bits; often some sort of count
+    flag_t have_status; // set to true by the device when operation is complete
+    int major;
+    int substatus;
+    flag_t is_read;
+    int time; // request by device for queuing via sim_activate()
+} chan_devinfo;
+
