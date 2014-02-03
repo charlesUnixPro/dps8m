464985315d395d653c25db6dee5be1703ce5a9d3
Charles Anthony <charles.unix.pro@gmail.com>
2014-02-03 15:41:01 -0800

    Fix comment

--- src/tapeUtils/mfile.c
+++ src/tapeUtils/mfile.c
@@ -1,5 +1,5 @@
-// Multics file command. Take a list of .tap files created by 
-// extract_tape_files.c, look at the header of each, and try to determine
+// Multics file command. Take a list of .dat files created by 
+// extract_tape_files, look at the header of each, and try to determine
 // what kind of file it is.
 
 #include <stdio.h>
