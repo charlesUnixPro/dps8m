11587b595c5750b199ab336d82bd4d2f41e3a4a2
Charles Anthony <charles.unix.pro@gmail.com>
2014-02-15 15:49:06 -0800

    Only list source code when DGB_TRACE is set

--- src/dps8/dps8_ins.c
+++ src/dps8/dps8_ins.c
@@ -696,7 +696,8 @@ t_stat executeInstruction(DCDstruct *ci)
             if (where)
               {
                 sim_debug(DBG_TRACE, &cpu_dev, "[%lld] %05o:%06o %s\n", cpuCycles, PPR.PSR, rIC, where);
-                listSource (compname, compoffset);
+                if_sim_debug (DBG_TRACE, &cpu_dev)
+                  listSource (compname, compoffset);
               }
             sim_debug(DBG_TRACE, &cpu_dev, "[%lld] %05o:%06o (%08o) %012llo (%s) %06o %03o(%d) %o %o %o %02o\n", cpuCycles, PPR.PSR, rIC, finalAddress, IWB, disAssemble(IWB), address, opcode, opcodeX, a, i, GET_TM(tag) >> 4, GET_TD(tag) & 017);
         }
