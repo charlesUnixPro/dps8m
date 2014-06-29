ccf6194702ac2f8c327e2c2b14db8f7545e4113f
Charles Anthony <charles.unix.pro@gmail.com>
2014-06-29 16:31:58 -0700

    Add word72 typedef.

--- src/tapeUtils/bit36.h
+++ src/tapeUtils/bit36.h
@@ -1,7 +1,9 @@
 typedef uint16_t word9;
 typedef uint32_t word18;
 typedef uint64_t word36;
+typedef uint64_t word36;
 typedef unsigned int uint;
+typedef __uint128_t word72;
 
 word36 extr36 (uint8_t * bits, uint woffset);
 word9 extr9 (uint8_t * bits, uint coffset);
