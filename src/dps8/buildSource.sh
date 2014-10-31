c3417433fb2d0c7b076f8661f5c0ef47b854e840
Charles Anthony <charles.unix.pro@gmail.com>
2014-10-31 13:30:04 -0700

    Add .cds files to the tree.

--- src/dps8/buildSource.sh
+++ src/dps8/buildSource.sh
@@ -4,7 +4,7 @@ T=./source/
 S=./MR12.3/
 
 # *.alm, *.pl1
-(cd $S && find . -type f \( -name "*.alm" -o -name "*.pl1" -o -name "*.info" \)  -print0) | xargs -0 -n 1 -I{} ../tapeUtils/p72_to_acsii $S/'{}' $T/'{}'
+(cd $S && find . -type f \( -name "*.alm" -o -name "*.pl1" -o -name "*.info" -o -name "*.cds" \)  -print0) | xargs -0 -n 1 -I{} ../tapeUtils/p72_to_acsii $S/'{}' $T/'{}'
 
 # *.s.archive
 (cd $S && find . -type f -name "*.s.archive" -print0) | xargs -0 -n 1 -I{} ../tapeUtils/p72archive_to_acsii $S/'{}' $T/'{}'
