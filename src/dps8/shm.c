dcfa1bb4f76c91e4959c0807f830e1c72e4c9edd
Charles Anthony <charles.unix.pro@gmail.com>
2014-12-10 06:32:43 -0800

    Add shm.[ch], the shared memory utiilities, and enable blinkenLights build.

---
+++ src/dps8/shm.c
@@ -0,0 +1,105 @@
+// shared memory library
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/mman.h>
+#include <sys/stat.h>        /* For mode constants */
+#include <fcntl.h>           /* For O_* constants */
+#include <unistd.h>
+#include <sys/types.h>
+
+//#include "dps8.h"
+//#include "dps8_utils.h"
+
+#define MAX_SEGS 256
+static int nsegs = 0;
+static char * seglist [MAX_SEGS];
+
+static void cleanup (void)
+  {
+    //printf ("cleanup\n");
+    for (int i = 0; i < nsegs; i ++)
+      {
+        //printf ("  unlink %s\n", seglist [i]);
+        shm_unlink (seglist [i]);
+      }
+  }
+
+static void addseg (char * name)
+  {
+    if (nsegs >= MAX_SEGS)
+      return;
+    seglist [nsegs ++] = strdup (name);
+    if (nsegs == 1)
+     atexit (cleanup);
+  }
+
+void * create_shm (char * key, pid_t system_pid, size_t size)
+  {
+    void * p;
+    char buf [256];
+    sprintf (buf, "/dps8m.%u.%s", system_pid, key);
+    int fd = shm_open (buf, O_CREAT | O_RDWR | O_EXCL, S_IRUSR | S_IWUSR);
+    if (fd == -1)
+      {
+        //sim_printf ("create_shm shm_open fail %d\n", errno);
+        return NULL;
+      }
+
+    if (ftruncate (fd, size) == -1)
+      {
+        //sim_printf ("create_shm  ftruncate  fail %d\n", errno);
+        return NULL;
+      }
+
+    p = mmap (NULL, size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+
+    if (p == MAP_FAILED)
+      {
+        //sim_printf ("create_shm mmap  fail %d\n", errno);
+        return NULL;
+      }
+    addseg (buf);
+
+    return p;
+  }
+
+void * open_shm (char * key, pid_t system_pid, size_t size)
+  {
+    void * p;
+    char buf [256];
+    sprintf (buf, "/dps8m.%u.%s", system_pid, key);
+    // Try to create it; it sucessful, then something is wrong, it should
+    // already exist
+    int fd = shm_open (buf, O_CREAT | O_RDWR | O_EXCL, S_IRUSR | S_IWUSR);
+    if (fd != -1)
+      {
+        close (fd);
+        //printf ("open_shm shm_open fail %d\n", errno);
+        return NULL;
+      }
+
+    fd = shm_open (buf, O_RDWR, S_IRUSR | S_IWUSR);
+    if (fd == -1)
+      {
+        close (fd);
+        //printf ("open_shm shm_open fail %d\n", errno);
+        return NULL;
+      }
+
+    if (ftruncate (fd, size) == -1)
+      {
+        //printf ("open_shm ftruncate  fail %d\n", errno);
+        return NULL;
+      }
+
+    p = mmap (NULL, size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+    if (p == MAP_FAILED)
+      {
+        //printf ("open_shm mmap  fail %d\n", errno);
+        return NULL;
+      }
+    addseg (buf);
+    return p;
+  }
---
+++ src/dps8/shm.h
@@ -0,0 +1,3 @@
+void * create_shm (char * key, pid_t system_pid, size_t size);
+void * open_shm (char * key, pid_t system_pid, size_t size);
+
