2283c6d0e7233d370c08adb85766265db0389a06
Charles Anthony <charles.unix.pro@gmail.com>
2016-06-05 17:01:08 -0700

    Make the single FNP mailbox canonical for now.

--- src/dps8/dps8_fnp.c
+++ src/dps8/dps8_fnp.c
@@ -737,7 +737,9 @@ static void dmpmbx (uint mailboxAddress)
 static int findMbx (uint fnpUnitNumber)
   {
     struct fnpUnitData * fudp = & fnpUnitData [fnpUnitNumber];
-    for (uint i = 0; i < 4; i ++)
+// See comment at top of file
+    //for (uint i = 0; i < 4; i ++)
+    for (uint i = 0; i < 1; i ++)
       if (! fudp -> fnpMBXinUse [i])
         return i;
     return -1;
