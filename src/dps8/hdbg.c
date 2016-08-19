f6896303cb5f8ba832a9c783e5477bcb5844b766
Charles Anthony <charles.unix.pro@gmail.com>
2016-08-19 05:30:52 -0700

    hdbg.c: Fix include files.

--- src/dps8/hdbg.c
+++ src/dps8/hdbg.c
@@ -242,7 +242,6 @@ t_stat hdbg_size (UNUSED int32 arg, char * buf)
     return SCPE_OK;
   }
 #else
-#include "dps8_utils.h"
 t_stat hdbg_size (UNUSED int32 arg, UNUSED char * buf)
   {
     sim_printf ("hdbg not enabled; ignoring\n");
