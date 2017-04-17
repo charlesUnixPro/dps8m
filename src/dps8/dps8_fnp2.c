7ca073ce137ce2f1e7a676449bbcccf1aeeb0ff2
Charles Anthony <charles.unix.pro@gmail.com>
2017-04-17 14:02:32 -0700

    Fix HSLA BS/CR sigabrt (ticket #129).

--- src/dps8/dps8_fnp2.c
+++ src/dps8/dps8_fnp2.c
@@ -2864,9 +2864,12 @@ void processUserInput (uv_tcp_t * client, unsigned char * buf, ssize_t nread)
                 case '\b':  // backspace
                 case 127:   // delete
                   {
-                    fnpuv_start_writestr (client, "\b \b");    // remove char from line
-                    p->buffer[p->nPos] = 0;     // remove char from buffer
-                    p->nPos -= 1;                 // back up buffer pointer
+                    if (p->nPos)
+                      {
+                        fnpuv_start_writestr (client, "\b \b");    // remove char from line
+                        p->buffer[p->nPos] = 0;     // remove char from buffer
+                        p->nPos -= 1;                 // back up buffer pointer
+                      }
                   }
                   break;
 
@@ -2903,9 +2906,12 @@ void processUserInput (uv_tcp_t * client, unsigned char * buf, ssize_t nread)
                 case '\b':  // backspace
                 case 127:   // delete
                   {
-                    fnpuv_start_writestr (client, "\b \b");    // remove char from line
-                    p->buffer[p->nPos] = 0;     // remove char from buffer
-                    p->nPos -= 1;                 // back up buffer pointer
+                    if (p->nPos)
+                      {
+                        fnpuv_start_writestr (client, "\b \b");    // remove char from line
+                        p->buffer[p->nPos] = 0;     // remove char from buffer
+                        p->nPos -= 1;                 // back up buffer pointer
+                      }
                   }
                   break;
 
