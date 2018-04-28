299445f37ca3b9cad8e677730e587c10bbfbb758
Juergen Weiss <weiss@uni-mainz.de>
2018-04-28 18:41:12 +0200

    require even address for its/itp mark rmw cycle for special zoned writes

--- src/dps8/dps8_addrmods.c
+++ src/dps8/dps8_addrmods.c
@@ -525,6 +525,7 @@ startCA:;
         // in case it turns out to be a ITS/ITP
         iTAG = cpu.rTAG;
 
+        word18 saveCA = cpu.TPR.CA;
         ReadIndirect ();
 
         // "In the case of RI modification, only one indirect reference is made
@@ -554,7 +555,15 @@ startCA:;
               }
           }
 
-        if (ISITP (cpu.itxPair[0]) || ISITS (cpu.itxPair[0]))
+        if ((saveCA & 1) == 1 && (ISITP (cpu.itxPair[0]) || ISITS (cpu.itxPair[0])))
+	  {
+	    sim_warn ("%s: itp/its at odd address\n", __func__);
+#ifdef TESTING
+	    traceInstruction (0);
+#endif
+	  }
+
+	if ((saveCA & 1) == 0 && (ISITP (cpu.itxPair[0]) || ISITS (cpu.itxPair[0])))
           {
             do_ITS_ITP (iTAG, & cpu.rTAG);
           }
@@ -604,7 +613,15 @@ startCA:;
         word18 saveCA = cpu.TPR.CA;
         ReadIndirect ();
 
-        if (ISITP (cpu.itxPair[0]) || ISITS (cpu.itxPair[0]))
+        if ((saveCA & 1) == 1 && (ISITP (cpu.itxPair[0]) || ISITS (cpu.itxPair[0])))
+	  {
+	    sim_warn ("%s: itp/its at odd address\n", __func__);
+#ifdef TESTING
+	    traceInstruction (0);
+#endif
+	  }
+
+	if ((saveCA & 1) == 0 && (ISITP (cpu.itxPair[0]) || ISITS (cpu.itxPair[0])))
           {
             do_ITS_ITP (iTAG, & cpu.rTAG);
           }
@@ -904,7 +921,12 @@ startCA:;
 
                 cpu.cu.pot = 1;
 
+#ifdef LOCKLESSXXX
+		// gives warnings as another lock is aquired in between
+                Read (cpu.TPR.CA, & cpu.ou.character_data, (i->info->flags & RMW) == STORE_OPERAND ? OPERAND_RMW : OPERAND_READ);
+#else
                 Read (cpu.TPR.CA, & cpu.ou.character_data, OPERAND_READ);
+#endif
 #ifdef LOCKLESS
 		cpu.char_word_address = cpu.iefpFinalAddress;
 #endif
